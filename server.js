32a71bfb7ccc53169659b23e6871c37de73b5ba2
GUL-Bao <BaoPham92@yahoo.com>
2019-10-30 13:29:43 -0400

    Adjusted gateKeeper

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -13,10 +14,24 @@ function logMeth(req, res, next) {
   next();
 }
 
+function gateKeeper(req, res, next) {
+    const password = req.headers.password || '';
+
+    if (password && password.toLowerCase() === 'mellon') {
+      next();
+    } else if (!!password === false) {
+      res.status(400).json({ message: "provide password"})
+    } else {
+      res.status(401).json({ you: "Shall not pass" })
+    }
+}
+
+server.use(gateKeeper)
 server.use(helmet());
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
+server.use(morgan('dev'))
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
